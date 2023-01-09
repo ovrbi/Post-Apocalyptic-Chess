2bf0c7f37992641c0a9b56eb0af8d38db1f08d74
rubr <3k1aire@gmail.com>
2023-01-09 04:45:23 +0200

    turn number

--- Scripts/Turn Number.gd
+++ Scripts/Turn Number.gd
@@ -1,14 +1,25 @@
 extends Label
 
+var tilemap
+var isready = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	tilemap = $/root/MainScene/Board
+	print(tilemap)
+	isready = true
+	_update()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-func update():
-	text = "TURN #" + 
+
+func _update():
+	if isready:
+		text = "TURN #" + str(tilemap.turn)
+
+
+func _on_board_next_turn():
+	_update()
