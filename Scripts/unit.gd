0c8c2806d5565ed2907cfbe165d66f9966546814
Iris <ingussm.is@gmail.com>
2023-01-09 16:28:52 +0200

    almost lose

--- Scripts/unit.gd
+++ Scripts/unit.gd
@@ -17,12 +17,15 @@ var rooted = false
 var atk_dir
 var alpha_amount = 0.5
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	tilemap = get_parent()
+
 	curhp = maxhp
 	if type == 0:
 		modulate.a = alpha_amount
+		tilemap.friendlies_alive+=1
 	update_label()
 
 
@@ -95,6 +98,10 @@ func check_adjacent(loc:Vector2i, plant:bool):
 		return tilemap.check_friendly(loc) && tilemap.get_units(loc)[0].curhp>0
 
 func die():
+	if type==0:
+		tilemap.friendlies_alive-=1
+		if tilemap.friendlies_alive<=0:
+			get_tree().change_scene_to_file("res://Scenes/lose_screen.tscn")
 	queue_free()
 
 func move_to(to : Vector2i):
@@ -138,6 +145,7 @@ func get_moves():
 	var locs = []
 	var to_process = [tilemap.local_to_map(position)]
 	var moves = [move_amount+tilemap.ms_boost*int(type==1)]
+	if rooted: moves = [0]
 	while !to_process.is_empty() && moves[0]>=0:
 		var delta = Vector2i(1,0)
 		for i in range(4):
